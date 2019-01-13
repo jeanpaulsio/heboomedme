e00fd61e8fb63191202abbf8159b1e3e66732f7d
jeanpaulsio <sio.jeanpaul@gmail.com>
2019-01-12 20:35:07 -0800

    tab thru inputs and copy to clipboard

--- src/App.js
+++ src/App.js
@@ -1,20 +1,84 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
+const EDITABLE_FIELDS = [1, 3, 4, 5, 7, 9, 11, 13, 15, 17, 19, 21];
+
 class App extends Component {
+  state = {
+    lyrics: [
+      "Overheard in",
+      "Cavs locker room", // 1
+      "after",
+      "Game 7.", //
+      '"He got me"', //
+      "Lebron", //
+      "said of",
+      "Tatum's dunk over him.", //
+      "That f***ing",
+      "Tatum", //
+      "boomed me.",
+      "Lebron", //
+      "added",
+      '"He\'s so good",', //
+      "repeating it four times.",
+      "Lebron", //
+      "then said",
+      "he", //
+      "wanted to add",
+      "Tatum", //
+      "to the list of",
+      "players he works out with", //
+      "this summer.",
+    ],
+  };
+
+  copyToClipboard = () =>
+    navigator.clipboard.writeText(this.state.lyrics.join(" "));
+
+  handleChange = (e, index) => {
+    // lebron is 5, 11, 15
+    // tatum is 9, 19
+    const newLine = e.target.value;
+    const newLyrics = this.state.lyrics.map((lyric, i) => {
+      if (i === index) {
+        return newLine;
+      }
+
+      return lyric;
+    });
+
+    this.setState({
+      lyrics: newLyrics,
+    });
+  };
+
   render() {
     return (
-      <div className="App">
+      <div className="main">
         <h1>He Boomed Me</h1>
-        <p>Overheard in Cavs locker room after Game 7</p>
-        <p>"He got me", Lebron said of Tatum's dunk over him.</p>
-        <p>"That f***ing Tatum boomed me."</p>
-        <p>Lebron added, "He's so good," repeating it four times</p>
-        <p>
-          Lebron then said he wanted to add Tatum to the list of players he
-          works out with this summer
-        </p>
+        {this.state.lyrics.map((line, index) => {
+          if (EDITABLE_FIELDS.includes(index)) {
+            return (
+              <input
+                className="editable-line"
+                key={index}
+                placeholder={line}
+                onChange={e => this.handleChange(e, index)}
+              />
+            );
+          }
+
+          return (
+            <span key={index} className="line">
+              {line}
+            </span>
+          );
+        })}
+        <div>
+          <button className="button" onClick={this.copyToClipboard}>
+            copy
+          </button>
+        </div>
       </div>
     );
   }
